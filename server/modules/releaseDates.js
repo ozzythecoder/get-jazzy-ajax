6498cd5c23d6e55535ce0f626569058a8fcad217
August McAllister <august.chm@gmail.com>
2022-11-02 15:19:33 -0500

    Added album release date array & sourced into the DOM

---
+++ server/modules/releaseDates.js
@@ -0,0 +1,16 @@
+const releaseDates = [
+  {
+    title: 'Kind of Blue',
+    year: 1959
+  },
+  {
+    title: 'Ella and Louis',
+    year: 1956
+  },
+  {
+    title: 'A Love Supreme',
+    year: 1956
+  }
+];
+
+module.exports = releaseDates;
