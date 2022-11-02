8325b41888836becda46cb0b88cedd0654a52939
August McAllister <august.chm@gmail.com>
2022-11-02 15:09:01 -0500

    Restructured files

--- server/server.js
+++ server/server.js
@@ -3,47 +3,50 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-const artistListArray = [
-    {
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    },
-];
+const artistListArray = require('./modules/artist.js')
+const songListArray = require('./modules/song.js')
 
-const songListArray = [
-    {
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
-];
+// const artistListArray = [
+//     {
+//         name: 'Miles Davis',
+//         born: 1926,
+//         died: 1990,
+//     },
+//     {
+//         name: 'Duke Ellington',
+//         born: 1899,
+//         died: 1974,
+//     },
+//     {
+//         name: 'John Coltrane',
+//         born: 1926,
+//         died: 1987,
+//     },
+//     {
+//         name: 'Louis Daniel Armstrong',
+//         born: 1901,
+//         died: 1971,
+//     },
+// ];
+
+// const songListArray = [
+//     {
+//         title: 'Take Five',
+//         artist: 'The Dave Brubeck Quartet',
+//     },
+//     {
+//         title: 'So What',
+//         artist: 'Miles Davis',
+//     },
+//     {
+//         title: 'Sing Sing Sing',
+//         artist: 'Benny Goodman',
+//     },
+//     {
+//         title: 'Take the "A" Train',
+//         artist: 'The Dave Brubeck Quartet',
+//     },
+// ];
 
 app.use(express.static('server/public'));
 
