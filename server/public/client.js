e8cde47e1af10fd1dfee7dc30f723b7be457fa8d
August McAllister <august.chm@gmail.com>
2022-11-02 15:04:04 -0500

    adding files to repo

---
+++ server/public/client.js
@@ -0,0 +1,39 @@
+$(document).ready(onReady);
+
+function onReady() {
+  // when the DOM loads, ajax will make a GET request at /artist
+  // which express will respond to (server.js:51)
+  $.ajax({
+    type: 'GET',
+    url: '/artist'
+  }).then(function (response) { // if this request is responded to affirmatively, THEN:
+    for (let i = 0; i < response.length; i++) { // loop over the returned array,
+      let artist = response[i]; // get the value of each artist in the array,
+      // and append it to the DOM. (whew!)
+      $('#artistTableBody').append(`
+        <tr>
+          <td>${artist.name}</td>
+          <td>${artist.born}</td>
+          <td>${artist.died}</td>
+        </tr>
+      `) // end append
+    } // end for loop
+  }) // end ajax
+
+  // ajax request for song data
+  $.ajax({ // send GET request at /song
+    type: 'GET',
+    url: '/song'
+  }).then(function (response) { // if request is responded to affirmatively, then:
+    for (let song of response) { // loop over array,
+      // and append each song to the DOM.
+      $('#songTableBody').append(`
+        <tr>
+          <td>${song.title}</td>
+          <td>${song.artist}</td>
+        </tr>
+      `) // end append
+    } // end for loop
+  }) // end ajax
+
+}
---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+const artistListArray = [
+    {
+        name: 'Miles Davis',
+        born: 1926,
+        died: 1990,
+    },
+    {
+        name: 'Duke Ellington',
+        born: 1899,
+        died: 1974,
+    },
+    {
+        name: 'John Coltrane',
+        born: 1926,
+        died: 1987,
+    },
+    {
+        name: 'Louis Daniel Armstrong',
+        born: 1901,
+        died: 1971,
+    },
+];
+
+const songListArray = [
+    {
+        title: 'Take Five',
+        artist: 'The Dave Brubeck Quartet',
+    },
+    {
+        title: 'So What',
+        artist: 'Miles Davis',
+    },
+    {
+        title: 'Sing Sing Sing',
+        artist: 'Benny Goodman',
+    },
+    {
+        title: 'Take the "A" Train',
+        artist: 'The Dave Brubeck Quartet',
+    },
+];
+
+app.use(express.static('server/public'));
+
+// when ajax makes a GET request at /artist, this function will run:
+app.get('/artist', (req, res) => {
+    res.send(artistListArray);
+});
+
+
+app.get('/song', (req, res) => {
+  res.send(songListArray);
+})
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+});
